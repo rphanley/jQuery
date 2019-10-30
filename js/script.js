18e7d61731ed5d9356a8a979f868f5b891090d52
rphanley <rphanley@rocketmail.com>
2019-10-30 17:35:51 +0000

    2 button slide and fade effects

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+ //
+ // add your jQuery code here
+    $("#button1").click(function(){
+            $("#para1").hide(2000).show(2000);
+
+        });
+
+
+        $("#button2").click(function(){
+            $("#para2").fadeOut().fadeIn();
+
+        });
+
+
+
+});
